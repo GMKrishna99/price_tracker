103780566b535ea0555034d0dce2084c3816d88a
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-09 13:13:52 +0530

    chore : added function to get images for hero carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+'use client'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Image from "next/image";
+
+const heroImages = [
+    { imgUrl: '/assets/images/hero-1.svg', alt: 'smart watch' },
+    { imgUrl: '/assets/images/hero-2.svg', alt: 'bag' },
+    { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp' },
+    { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer' },
+    { imgUrl: '/assets/images/hero-5.svg', alt: 'chair' },
+]
 
 const HeroCarousel = () => {
     return (
-        <div>HeroCarousel</div>
+        <div>
+           
+        </div>
     )
 }
 
