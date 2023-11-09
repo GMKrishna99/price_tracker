3ca7c12fe626c1613824bc249c9a5fd01631d183
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-09 13:40:15 +0530

    chore : track the entered url

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,12 +1,18 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Searchbar = () => {
+    const [SearchPrompt, setSearchPrompt] = useState('');
 
     const handleSubmit = () => { }
     return (
         <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
-            <input type="text" placeholder='Enter product link' className='searchbar-input' />
+            <input
+                type="text"
+                placeholder='Enter product link'
+                className='searchbar-input'
+                value={SearchPrompt}
+                onChange={(e) => setSearchPrompt(e.target.value)} />
             <button type='submit' className='searchbar-btn'>Search</button>
         </form>
     )
