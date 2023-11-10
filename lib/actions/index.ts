5d029da1cc31c2f9d8f09e2270b98f39f5fb00ca
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-10 11:00:41 +0530

    chore : check the link and scrape product

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,1 +1,16 @@
 "use server";
+
+import { scrapeAmazonProduct } from "../scraper";
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+  // check if it doesn't exist
+  if (!productUrl) return;
+
+  //   if do have a product
+  try {
+    // scraped product
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product:${error.message}`);
+  }
+}
