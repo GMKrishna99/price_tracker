8e0aee6e31d728a10521e49451cbc643184549ed
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-17 09:55:41 +0530

    chore : extract the values of possible elements

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,36 @@
+// import { PriceHistoryItem, Product } from "@/types";
+
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
+
+const THRESHOLD_PERCENTAGE = 40;
+
+// Extracts and returns the price from a list of possible elements.
 export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
 
-    if (priceText) return priceText.replace(/[^\d.]/g, "");
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
+    }
   }
+
   return "";
+}
+
+// Extracts and returns the currency symbol from an element.
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
 }
