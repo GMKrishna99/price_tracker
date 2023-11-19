c02657f104b2253f874af6a8f892585d654a0299
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-19 12:30:37 +0530

    commented email notifi for feature use

--- lib/utils.ts
+++ lib/utils.ts
@@ -78,22 +78,22 @@ export function getAveragePrice(priceList: PriceHistoryItem[]) {
   const averagePrice = sumOfPrices / priceList.length || 0;
   return averagePrice;
 }
-export const getEmailNotifType = (
-  scrapedProduct: Product,
-  currentProduct: Product
-) => {
-  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
-  if (scrapedProduct.currentPrice < lowestPrice) {
-    return Notification.LOWEST_PRICE as keyof typeof Notification;
-  }
-  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
-    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
-  }
-  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
-    return Notification.THRESHOLD_MET as keyof typeof Notification;
-  }
-  return null;
-};
+// export const getEmailNotifType = (
+//   scrapedProduct: Product,
+//   currentProduct: Product
+// ) => {
+//   const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+//   if (scrapedProduct.currentPrice < lowestPrice) {
+//     return Notification.LOWEST_PRICE as keyof typeof Notification;
+//   }
+//   if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+//     return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+//   }
+//   if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+//     return Notification.THRESHOLD_MET as keyof typeof Notification;
+//   }
+//   return null;
+// };
 export const formatNumber = (num: number = 0) => {
   return num.toLocaleString(undefined, {
     minimumFractionDigits: 0,
