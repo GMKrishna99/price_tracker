d7e416bfc982dbd5638cfc9096adbc9d12f6cbcc
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-21 08:47:26 +0530

    chore : added existing product with duplicate and get product by ID

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import Product from "../models/Product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { revalidatePath } from "next/cache";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   // check if it doesn't exist
@@ -17,8 +20,38 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     if (!scrapedProduct) return;
 
     let product = scrapedProduct;
-    // const existingProduct = await Product
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      // modifying the product object
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Failed to create/update product:${error.message}`);
   }
+}
+
+// get product by ID
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+  } catch (error) {}
 }
