9c1cc9b2653a5cd879891c79034ab0d17b63501c
GMKrishna99 <gmohankrishna23@gmail.com>
2023-11-25 09:58:31 +0530

    chore : add user email to product actions

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { User } from "@/types";
 import Product from "../models/Product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
@@ -86,6 +87,28 @@ export async function getSimilarProducts(productId: string) {
       _id: { $ne: productId },
     }).limit(3);
     return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addUserEmailToProduct(
+  productId: string,
+  userEmail: string
+) {
+  try {
+    const product = await Product.findById(productId);
+    if (!product) return;
+
+    const userExists = product.users.some(
+      (user: User) => user.email === userEmail
+    );
+    if (!userExists) {
+      product.users.push({ email: userEmail });
+      await product.save();
+
+      const emailContent = generateEmailBody(product, "WELCOME");
+    }
   } catch (error) {
     console.log(error);
   }
